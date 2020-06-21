60c95c409a47108969fd365c82a49040820b090c
Tiago H. Piantavinha <tiago.henrip@hotmail.com>
2020-06-21 19:48:16 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe('Suite de Teste de Exemplo com NodeJs', () => {
+    it('Test demonstrar o uso do Jasmine com nodeJs', () => {
+        expect(true).toEqual(true);
+    })
+
+    it('Deve demonstrar o uso do Karma em modo de escuta', () => {
+        expect(1).toEqual(1);
+    })
+})
